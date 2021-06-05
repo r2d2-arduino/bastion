0a4edf855d4034086bbf680606af96c387ed6cb4
r2d2-arduino <artourd@gmail.com>
2021-06-05 14:59:18 +0300

    sensor update

--- views/sensor/_form.php
+++ views/sensor/_form.php
@@ -12,8 +12,6 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'created')->textInput() ?>
-
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'shortname')->textInput(['maxlength' => true]) ?>
