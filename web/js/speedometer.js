7b03c440b65145826787f3c05dea5b0814646865
r2d2-arduino <artourd@gmail.com>
2021-06-07 23:19:40 +0300

    fix speedometer alive

--- web/js/speedometer.js
+++ web/js/speedometer.js
@@ -27,27 +27,30 @@ function getLastSensorsValue(sensor_id = 0, device_id = 0)
             
             for (var i = 0; i < data.length; i++)
             {
-                min = $('#sensor_'+data[i].sensor_id).data('min');
-                max = $('#sensor_'+data[i].sensor_id).data('max');
-                oldGrad = $('#sensor_'+data[i].sensor_id).data('grad');
-                
-                //console.log(data[i]);
-                value = data[i].value;
-                if (value > 100)
+                if (data[i].actuality < 100)
                 {
-                    value = Math.round(value);
+                    min = $('#sensor_'+data[i].sensor_id).data('min');
+                    max = $('#sensor_'+data[i].sensor_id).data('max');
+                    oldGrad = $('#sensor_'+data[i].sensor_id).data('grad');
+
+                    //console.log(data[i]);
+                    value = data[i].value;
+                    if (value > 100)
+                    {
+                        value = Math.round(value);
+                    }
+                    else
+                    {
+                        value = Math.round(value * 10) / 10;
+                    }
+
+                    newGrad = Math.round(180 * (value - min) / (max - min));
+
+                    $('#sensor_'+data[i].sensor_id).data('grad', newGrad);
+                    $('#sensor_'+data[i].sensor_id +' .number').html(value);
+                    $('#sensor_'+data[i].sensor_id +'').parent().addClass('alive');
+                    animateNeedleRotate(data[i].sensor_id, oldGrad, newGrad);    
                 }
-                else
-                {
-                    value = Math.round(value * 10) / 10;
-                }
-                
-                newGrad = Math.round(180 * (value - min) / (max - min));
-                
-                $('#sensor_'+data[i].sensor_id).data('grad', newGrad);
-                $('#sensor_'+data[i].sensor_id +' .number').html(value);
-                $('#sensor_'+data[i].sensor_id +'').parent().addClass('alive');
-                animateNeedleRotate(data[i].sensor_id, oldGrad, newGrad);    
             }
         },
         error: function (exception) {
