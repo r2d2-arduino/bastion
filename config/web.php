0ae057f24ca327cbfae0028eb697cba65c0d1b68
r2d2-arduino <artourd@gmail.com>
2021-06-07 20:28:44 +0300

    set pretty url

--- config/web.php
+++ config/web.php
@@ -47,6 +47,10 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                //'post/<id:\d+>' => 'post/view',
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
             ],
         ],
     ],
